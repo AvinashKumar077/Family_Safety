b4b4027e34796a48e46bbe7da662565e9acaf822
Avinash Kumar <94068118+AvinashKumar077@users.noreply.github.com>
2023-10-20 00:12:56 +0530

    initial commit

--- app/src/main/java/com/example/securefamily/MainActivity.kt
+++ app/src/main/java/com/example/securefamily/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.securefamily
+
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.securefamily.databinding.ActivityMainBinding
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -18,72 +29,174 @@ class MainActivity : AppCompatActivity() {
 
     val permissions = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.READ_CONTACTS,
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_CONTACTS
     )
     val permissionCode = 78
 
-
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        askForPermission()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+        if (isAllPermissionsGranted()) {
+            if (isLocationEnabled(this)) {
+                setUpLocationListener()
+            } else {
+                showGPSNotEnabledDialog(this)
+            }
+        } else {
+            askForPermission()
+        }
+        binding.bottomBar.setOnItemSelectedListener { menuItem ->
 
-        bottomBar.setOnItemSelectedListener {
-
-            when (it.itemId) {
-                R.id.nav_guard -> {
-                    inflateFragment(GuardFragment.newInstance())
-                }
+            when (menuItem.itemId) {
                 R.id.nav_home -> {
                     inflateFragment(HomeFragment.newInstance())
                 }
                 R.id.nav_dashboard -> {
                     inflateFragment(MapsFragment())
                 }
+                R.id.nav_guard -> {
+                    inflateFragment(GuardFragment.newInstance())
+                }
                 R.id.nav_profile -> {
                     inflateFragment(ProfileFragment.newInstance())
                 }
             }
+
             true
         }
-        bottomBar.selectedItemId = R.id.nav_home
 
-        val auth = FirebaseAuth.getInstance().currentUser
-        val name = auth?.displayName.toString()
-        val mail = auth?.email.toString()
-        val phoneNumber = auth?.phoneNumber.toString()
-        val imageUrl = auth?.photoUrl.toString()
+        binding.bottomBar.selectedItemId = R.id.nav_home
+
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val name = currentUser?.displayName.toString()
+        val mail = currentUser?.email.toString()
+        val phoneNumber = currentUser?.phoneNumber.toString()
+        val imageUrl = currentUser?.photoUrl.toString()
+
 
         val db = Firebase.firestore
 
-        // Create a new user with a first and last name
         val user = hashMapOf(
             "name" to name,
             "mail" to mail,
             "phoneNumber" to phoneNumber,
-            "imageUrl" to imageUrl,
+            "imageUrl" to imageUrl
         )
-    db.collection("users").document(mail).set(user)
-        .addOnSuccessListener {  }
-        .addOnFailureListener {  }
-// Add a new document with a generated ID
 
+
+        db.collection("users").document(mail).set(user).addOnSuccessListener {
+
+        }.addOnFailureListener {
+
+        }
 
     }
+
+    private fun setUpLocationListener() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    for (location in locationResult.locations) {
+                        Log.d("Location89", "onLocationResult: latitude ${location.latitude}")
+                        Log.d("Location89", "onLocationResult: longitude ${location.longitude}")
+
+
+                        val currentUser = FirebaseAuth.getInstance().currentUser
+                        val mail = currentUser?.email.toString()
+
+                        val db = Firebase.firestore
+
+                        val locationData = mutableMapOf<String,Any>(
+                            "lat" to location.latitude.toString(),
+                            "long" to location.longitude.toString(),
+                        )
+
+
+                        db.collection("users").document(mail).update(locationData)
+                            .addOnSuccessListener {
+
+                            }.addOnFailureListener {
+
+                            }
+
+
+                    }
+                }
+            },
+            Looper.myLooper()
+        )
+    }
+
+    fun isLocationEnabled(context: Context): Boolean {
+        val locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    /**
+     * Function to show the "enable GPS" Dialog box
+     */
+    fun showGPSNotEnabledDialog(context: Context) {
+        AlertDialog.Builder(context)
+            .setTitle("Enable GPS")
+            .setMessage("required_for_this_app")
+            .setCancelable(false)
+            .setPositiveButton("enable_now") { _, _ ->
+                context.startActivity(Intent(ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+            .show()
+    }
+
+    private fun isAllPermissionsGranted(): Boolean {
+        for (item in permissions) {
+            if (ContextCompat
+                    .checkSelfPermission(
+                        this,
+                        item
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
 
     private fun askForPermission() {
-
-        ActivityCompat.requestPermissions(this,permissions,permissionCode)
+        ActivityCompat.requestPermissions(this, permissions, permissionCode)
     }
 
-    private fun inflateFragment(newInstance:Fragment) {
-
+    private fun inflateFragment(newInstance: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,newInstance)
+        transaction.replace(R.id.container, newInstance)
         transaction.commit()
     }
 
@@ -94,12 +207,16 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == permissionCode){
-            if(allPermissionGranted()){
+        if (requestCode == permissionCode) {
 
-            }else{
+            if (allPermissionGranted()) {
+                //openCamera()
+
+                setUpLocationListener()
+            } else {
 
             }
+
         }
     }
 
@@ -109,11 +226,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun allPermissionGranted(): Boolean {
-        for(item in permissions){
-            if(ContextCompat.checkSelfPermission(this,item) != PackageManager.PERMISSION_GRANTED ){
+        for (item in permissions) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    item
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
                 return false
             }
         }
+
         return true
     }
+
 }
